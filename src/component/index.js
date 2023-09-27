553adb8887d5e9ccfda3d2f6619b596359f0b6ca
code-shivam <shivamsingh01042001@gmail.com>
2023-09-27 13:22:36 +0530

    update code

--- src/component/index.js
+++ src/component/index.js
@@ -1,72 +1,54 @@
 import { GetDateMonthTime } from "../constant";
 
 function TaskList({
-    title,
-    tasks,
-    updateTaskStatus,
-    filterStatus,
-    filterClassName,
-  }) {
-    return (
-      <div className="subContainer">
-        <h1 className="heading">{title}</h1>
-        <div className="myContainer">
-          {tasks.map(
-            (task, index) =>
-              (filterStatus === undefined || task.status === filterStatus) && (
-                <div className={filterClassName} key={index}>
-                  <p className="title">{task.todo}</p>
-                  <p className="time">Time: {GetDateMonthTime(task.time)}</p>
-                  {task.status === "todo" && (
-                    <div className="buttonSection">
-                      <button className="prevButton" disabled>
-                        Prev
-                      </button>
-                      <button
-                        className="nextButton"
-                        onClick={() => updateTaskStatus(index, "inProgress")}
-                      >
-                        Next
-                      </button>
-                    </div>
-                  )}
-                  {task.status === "inProgress" && (
-                    <div className="buttonSection">
-                      <button
-                        className="prevButton"
-                        onClick={() => updateTaskStatus(index, "todo")}
-                      >
-                        Prev
-                      </button>
-                      <button
-                        className="nextButton"
-                        onClick={() => updateTaskStatus(index, "done")}
-                      >
-                        next
-                      </button>
-                    </div>
-                  )}
-                  {task.status === "done" && (
-                    <div className="buttonSection">
-                      <button
-                        className="prevButton"
-                        onClick={() => updateTaskStatus(index, "inProgress")}
-                      >
-                        Prev
-                      </button>
-                      <button className="nextButton" disabled>
-                        next
-                      </button>
-                    </div>
-                  )}
+  title,
+  tasks,
+  updateTaskStatus,
+  filterStatus,
+  filterClassName,
+}) {
+  return (
+    <div className="subContainer">
+      <h1 className="heading">{title}</h1>
+      <div className="myContainer">
+        {tasks.map(
+          (task, index) =>
+            (filterStatus === undefined || task.status === filterStatus) && (
+              <div className={filterClassName} key={index} draggable>
+                <p className="title">{task.todo}</p>
+                <p className="time">Time: {GetDateMonthTime(task.time)}</p>
+                <div className="buttonSection">
+                  <button
+                    className="prevButton"
+                    disabled={task.status === "todo"}
+                    onClick={() =>
+                      updateTaskStatus(
+                        index,
+                        task.status === "inProgress" ? "todo" : "inProgress"
+                      )
+                    }
+                  >
+                    Prev
+                  </button>
+                  <button
+                    className="nextButton"
+                    disabled={task.status === "done"}
+                    onClick={() =>
+                      updateTaskStatus(
+                        index,
+                        task.status === "todo" ? "inProgress" : "done"
+                      )
+                    }
+                  >
+                    Next
+                  </button>
                 </div>
-              )
-          )}
-        </div>
+              </div>
+            )
+        )}
       </div>
-    );
-  }
+    </div>
+  );
+}
 
-
-  export default TaskList
-  
+export default TaskList;
